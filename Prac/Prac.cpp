013d3bb6c2570a38b949e8ecc06566263a5cdd8f
Jeroen <49836368+jeroentyx@users.noreply.github.com>
2020-05-14 08:09:33 +0800

    Add project files.

---
+++ Prac/Prac.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+/*Substitution Cipher*/
+int main()
+{
+	std::string alpha{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+	std::string key{ "XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr" };
+
+	std::string user{};
+	std::string encrypt{};
+	std::string decrypt{};
+
+
+	std::cout << "Enter string " << std::endl;
+	std::getline(std::cin, user);
+
+	/*Check for the position*/
+	for (char c : user)
+	{
+		/*Find the character in the alpha*/
+		size_t position = alpha.find(c);
+		/*Check if there is character*/
+		if (position != std::string::npos)
+			encrypt += key.at(position);
+		else
+			encrypt += c;
+
+	}
+
+	std::cout << "Encrypted message is " << encrypt << std::endl;
+
+	for (char c : encrypt)
+	{
+		/*Find the position in the key*/
+		size_t position = key.find(c);
+		/*Check if there is character*/
+		if (position != std::string::npos)
+			decrypt += alpha.at(position);
+		else
+			decrypt += c;
+	}
+	std::cout << "Decrypted message is " << decrypt << std::endl;
+
+
+
+	return 0;
+}
